a682cad41863d58a85ae0d813ea7d4a4e433db07
AdrianoLima19 <adrianolima.dev@gmail.com>
2024-11-08 17:42:29 -0300

    Adiciona rotas API para registo, login, logout e perfil com autenticação de rotas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\JwtAuthMiddleware;
+use App\Http\Controllers\JwtAuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(JwtAuthController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+
+    Route::middleware(JwtAuthMiddleware::class)->group(function () {
+        Route::get('profile', 'profile');
+        Route::post('logout', 'logout');
+    });
+});
